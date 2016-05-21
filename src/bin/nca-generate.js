9900d3a5b57bc65a45859b03160029d62d88723e
SHIN Suzuki <shinout310@gmail.com>
2016-05-22 03:57:28 +0900

    add nca-generate subcommand

---
+++ src/bin/nca-generate.js
@@ -0,0 +1,26 @@
+/*eslint no-console: 0 */
+// @flow
+
+import fs from 'fs'
+import {join} from 'path'
+import chalk from 'chalk'
+import WorkingDirectory from '../lib/working-directory'
+import AutoreleaseYml from '../lib/autorelease-yml'
+import CircleYml from '../lib/circle-yml'
+
+export default function run() {
+
+    const rootDir = new WorkingDirectory().resolve()
+
+    const arYml = AutoreleaseYml.loadFromDir(rootDir)
+
+    const ymlStr = CircleYml.generate(arYml) 
+
+    const filename = join(rootDir, 'circle.yml')
+
+    fs.writeFileSync(filename, ymlStr)
+
+    console.log(chalk.green('circle.yml was successfully generated!'))
+}
+
+if (require.main === module) run()
