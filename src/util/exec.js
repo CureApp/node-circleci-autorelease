213c044d634e2277fba3ae07b73330e1289611ab
SHIN Suzuki <shinout310@gmail.com>
2016-05-26 18:27:57 +0900

    show info of exec result

--- src/util/exec.js
+++ src/util/exec.js
@@ -3,18 +3,33 @@
 import {exec as shellJSExec} from 'shelljs'
 import chalk from 'chalk'
 
+const CHECK_OK = '✓'
+const CHECK_NG = '✖'
+
 export default function exec(command: string, options?: Object = {}): Object {
 
     const dryRun = !!process.env.DRY_RUN
 
     options.silent = true
 
-    console.log(chalk.green(`${dryRun ? '[DRY RUN]': 'executing'} ${command}`))
-
     if (dryRun) {
-        return {command, stdout: '[DRY RUN]', stderr: '[DRY RUN]'}
+        console.log(chalk.yellow(`[DRY RUN]: ${command}`))
+        return {command, stdout: '[DRY RUN]', stderr: '[DRY RUN]', code: 0}
     }
     else {
-        return shellJSExec(command, options)
+        const result = shellJSExec(command, options)
+        const succeeded = result.code === 0
+        const color = succeeded ? 'green': 'red'
+        const check = succeeded ? CHECK_OK : CHECK_NG
+        console.log(chalk[color](` ${check}  ${command}`))
+
+        if (!succeeded) {
+            console.log('\tSTDOUT: ')
+            console.log(chalk.red(result.stdout))
+            console.error('\tSTDERR: ')
+            console.error(chalk.red(result.stderr))
+        }
+
+        return result
     }
 }
