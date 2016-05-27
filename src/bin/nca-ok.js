699b705ff29226edb9a9d61a0031a0a4e36a65ba
SHIN Suzuki <shinout310@gmail.com>
2016-05-27 15:18:08 +0900

    release 2.0.8

--- src/bin/nca-ok.js
+++ src/bin/nca-ok.js
@@ -6,8 +6,7 @@ import ReleasabilityChecker from '../lib/releasability-checker'
 export default function run() {
     const checker = new ReleasabilityChecker()
     const warnMessage = checker.check()
-    const code = warnMessage != '' ? 1 : 0
-
+    const code = warnMessage ? 1 : 0
     process.exit(code)
 }
 
